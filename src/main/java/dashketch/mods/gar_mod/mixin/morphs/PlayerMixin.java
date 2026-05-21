f8ae3f34864b4e8a2a294494c10f54ad30e4d4d7
Dash_Ketch <nateisthegreatgreat@gmail.com>
2026-05-21 14:40:05 -0700

    Change logger import to use local GAR_Mod LOGGER
    i js lwk didnt like the last commit on this file

--- src/main/java/dashketch/mods/gar_mod/mixin/morphs/PlayerMixin.java
+++ src/main/java/dashketch/mods/gar_mod/mixin/morphs/PlayerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.neoforged.neoforgespi.ILaunchContext.LOGGER;
+import static dashketch.mods.gar_mod.GAR_Mod.LOGGER;
 
 @Mixin(AbstractClientPlayer.class)
 public class PlayerMixin {
