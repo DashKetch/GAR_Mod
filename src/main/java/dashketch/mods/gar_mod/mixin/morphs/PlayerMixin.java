f257addfb87af17b3a2f823456c3e481ba303753
Dash_Ketch <nateisthegreatgreat@gmail.com>
2026-05-22 18:09:31 -0700

    fix logger import

--- src/main/java/dashketch/mods/gar_mod/mixin/morphs/PlayerMixin.java
+++ src/main/java/dashketch/mods/gar_mod/mixin/morphs/PlayerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static dashketch.mods.gar_mod.GAR_Mod.LOGGER;
+import static dashketch.mods.gar_mod.Gar_mod.LOGGER;
 
 @Mixin(AbstractClientPlayer.class)
 public class PlayerMixin {
